74a92a25efc8d68f301d194e69cb85bf5944040a
Fakir Muzaffar Nisar <98309599+Muzaffarfakir@users.noreply.github.com>
2024-05-04 19:40:25 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+let express = require("express");
+let app = express();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
+let port = process.env.PORT || 8080;
+let cors = require("cors");
+let config = {
+    origin: "http://localhost:3000",
+    credentials: true,
+
+}
+app.use(cors(config));
+app.get("/", (req, res) => {
+    res.send("hello");
+})
+//////ios
+let users = 0;
+io.on("connection", (socket) => {
+    //    //
+    socket.on("chat", (data, name) => {
+
+        console.log(data, name);
+        socket.emit("chat", data);
+
+        socket.broadcast.emit("chat", ` ${data} `, name)
+    });
+    socket.on("discoonect", () => {
+        users--;
+        io.emit("users", users)
+
+    })
+
+})
+//io.listen(http)
+http.listen(port);
